e829f75c53a55db685967eca8d9e8c0b56f0e50d
scheulenj@gmail.com <152833532+JScheulen@users.noreply.github.com>
2024-07-24 21:47:27 -0400

    cambio en el archivo wsgi para inicializar el modulo en el deploy

--- fncapp/wsgi.py
+++ fncapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fncapp.fncapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fncapp.settings')
 
 application = get_wsgi_application()
